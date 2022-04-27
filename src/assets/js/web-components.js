0ef7619321314fd721a4a858dbc985a4af35fa92
James Cavanagh <james.cavanagh@oneadvanced.com>
2022-04-27 16:09:41 +0100

    feat: update component paths

--- src/assets/js/web-components.js
+++ src/assets/js/web-components.js
@@ -1,20 +1,15 @@
 // import mosaic js
 import { use } from 'https://cdn.svc.oneadvanced.com/mosaicjs/latest/mosaic.esm.js'
 
-import { grid } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/grid/index.js'
+// import { dialog } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/dialog/v0/dialog.min.js'
 import { dialog } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/dialog/index.js'
-import { icon } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/icon/index.js'
-import { accordion } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/accordion/index.js'
-import { sheet } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/sheet/index.js'
-import { drawer } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/drawer/index.js'
-import { tabs } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/tabs/index.js'
-import { listGroup } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/list-group/index.js'
+import { drawer } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/drawer/v0/drawer.min.js'
+import { icon } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/icon/v0/icon.min.js'
+import { listGroup } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/list-group/v0/list-group.min.js'
+import { tabs } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/tabs/v0/tabs.min.js'
 
-use(grid)
 use(dialog)
-use(icon)
-use(accordion)
 use(drawer)
-use(sheet)
-use(tabs)
+use(icon)
 use(listGroup)
+use(tabs)
