7923d8a6b7fb699b50501de7d55fa2fa2aff0556
James Cavanagh <james.cavanagh@oneadvanced.com>
2022-04-28 10:36:36 +0100

    feat: update form component paths

--- src/assets/js/web-components.js
+++ src/assets/js/web-components.js
@@ -1,8 +1,7 @@
 // import mosaic js
 import { use } from 'https://cdn.svc.oneadvanced.com/mosaicjs/latest/mosaic.esm.js'
 
-// import { dialog } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/dialog/v0/dialog.min.js'
-import { dialog } from 'https://dev-cdn.svc.oneadvanced.io/tessera/components-core/v2/components/custom-elements/dialog/index.js'
+import { dialog } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/dialog/v0/dialog.min.js'
 import { drawer } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/drawer/v0/drawer.min.js'
 import { icon } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/icon/v0/icon.min.js'
 import { listGroup } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/list-group/v0/list-group.min.js'
