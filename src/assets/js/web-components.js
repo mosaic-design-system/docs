07b7311bf428883274c8d697fea6c1cf87947815
James Cavanagh <james.cavanagh@oneadvanced.com>
2022-05-26 12:19:06 +0100

    feat: update mds import paths

--- src/assets/js/web-components.js
+++ src/assets/js/web-components.js
@@ -1,11 +1,11 @@
 // import mosaic js
 import { use } from 'https://cdn.svc.oneadvanced.com/mosaicjs/latest/mosaic.esm.js'
 
-import { dialog } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/dialog/v0/dialog.min.js'
-import { drawer } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/drawer/v0/drawer.min.js'
-import { icon } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/icon/v0/icon.min.js'
-import { listGroup } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/list-group/v0/list-group.min.js'
-import { tabs } from 'https://dev-cdn.svc.oneadvanced.io/mosaic-design/tabs/v0/tabs.min.js'
+import { dialog } from 'https://cdn.svc.oneadvanced.com/mosaic-design-system/dialog/v1/dialog.min.js'
+import { drawer } from 'https://cdn.svc.oneadvanced.com/mosaic-design-system/drawer/v1/drawer.min.js'
+import { icon } from 'https://cdn.svc.oneadvanced.com/mosaic-design-system/icon/v1/icon.min.js'
+import { listGroup } from 'https://cdn.svc.oneadvanced.com/mosaic-design-system/list-group/v1/list-group.min.js'
+import { tabs } from 'https://cdn.svc.oneadvanced.com/mosaic-design-system/tabs/v1/tabs.min.js'
 
 use(dialog)
 use(drawer)
